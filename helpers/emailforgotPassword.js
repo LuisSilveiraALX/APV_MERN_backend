660fd2d58a75af01c158bfce85b758551a30e8a5
LuisSilveiraALX <lswagames@gmail.com>
2022-07-08 18:41:50 -0300

    first commit

---
+++ helpers/emailforgotPassword.js
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer';
+ 
+const emailForgotPassword = async (datos) => {
+    const {email, nombre, token} = datos;
+ 
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      });   
+ 
+    // Enviar email
+    const info = await transport.sendMail({
+        from: '"APV - Administrador Pacientes Veterinaria" <apv@correo.com>', // sender address
+        to: email, // list of receivers
+        subject: "APV - Reestablece tu Contraseña ✔", // Subject line
+        text: "Reestablece tu Contraseña ", // plain text body
+        html: ` <p> Hola <strong>${nombre}</strong> <br /> has solicitado reestablecer tu password.</p>
+
+        <p> haz click en el siguiente enlace para generar una nueva contraseña: 
+        <a href='${process.env.FRONTEND_URL}/olvide-password/${token}'>Reestablecer Password</a>
+        </p>
+        
+        <p>Si no creaste esta cuenta puedes eliminar este mensaje</p>
+ 
+        `, // html body
+      });
+ 
+    console.log('Mensaje enviado: %s', info.messageId);
+};
+ 
+export default emailForgotPassword;
